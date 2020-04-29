2127895d21c3a42becb8bca6084c87b500355487
Saiteju1997 <51901826+Saiteju1997@users.noreply.github.com>
2020-04-29 11:52:33 -0400

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,2 @@
+ls /var/lib/jenkins/workspace/jfrog-example/Bazinga/devops/ilp1/ILP/0.0.1-SNAPSHOT/*.war | tail -1 | xargs cp -t /var/lib/jenkins/workspace/jfrog-example/
+\rm -rf /var/lib/jenkins/workspace/jfrog-example/Bazinga/devops/ilp1/ILP/0.0.1-SNAPSHOT/*.war
